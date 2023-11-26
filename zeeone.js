bb00d2d622ccf5309fa1efd032c94e29aa74fa75
clevvbotz <108448307+clevvbotz@users.noreply.github.com>
2023-11-26 13:10:45 +0800

    Update zeeone.js

--- zeeone.js
+++ zeeone.js
@@ -2,6 +2,41 @@ const {
    spawn
 } = require('child_process')
 const path = require('path')
+const os = require('os');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+console.log('\x1b[33m%s\x1b[0m', `🌐 Port ${port} is open`);
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  const data = {
+    status: 'true',
+    message: 'Bot Successfully Activated!',
+    author: 'Anime Editz'
+  };
+  const result = {
+    response: data
+  };
+  res.send(JSON.stringify(result, null, 2));
+});
+
+function listenOnPort(port) {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+  app.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log(`Port ${port} is already in use. Trying another port...`);
+      listenOnPort(port + 1);
+    } else {
+      console.error(err);
+    }
+  });
+}
+
+listenOnPort(port);
 
 function start() {
    let args = [path.join(__dirname, 'alpha.js'), ...process.argv.slice(2)]
